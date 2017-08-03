359afb1a5476384feed9271e08d3a42b0dda2226
kvzaytsev <kirill.v.zaytsev@gmail.com>
2017-08-03 15:13:31 +0300

    subsets sum

---
+++ qv7j2/index.js
@@ -0,0 +1,14 @@
+const sum = array => array.reduce((res, el) => res += el, 0);
+const getSetForSum = (inputArray, targetSum) => {
+  let accumulators = []; 
+
+  inputArray.sort((a,b) => a-b);    
+  inputArray.forEach( el => {
+    accumulators = accumulators.filter(acc => sum(acc) <= targetSum);
+    accumulators = accumulators.concat(accumulators.map(acc => acc.concat(el)).concat([[el]]));
+  });
+  accumulators = accumulators.filter(acc => sum(acc) === targetSum);
+  return accumulators;
+};
+
+module.exports.getSetForSum = getSetForSum;
---
+++ test/qv7j2.spec.js
@@ -0,0 +1,36 @@
+'use strict'
+const chai = require('chai');
+const expect = chai.expect;
+const should = chai.should();
+
+const { getSetForSum } = require('../qv7j2/index');
+
+/*
+const targetSum = 20;
+
+while (inputArray.length !== 10) {
+    let num = Math.floor(Math.random()*20);
+    num !== 0 && !(inputArray.includes(num)) && inputArray.push(num);
+}*/ 
+
+describe('In array [16,17,11,3,13,10,6,7,12,9]', () => {
+  let srcArray = [16,17,11,3,13,10,6,7,12,9];
+  let targetSum = 20;
+  let expArrays = [
+    [3, 7, 10],
+    [3, 6, 11],
+    [9, 11],
+    [7, 13],
+    [3, 17]
+  ];
+
+  it(`should find proper sets`, () => {
+    let foundSets = getSetForSum(srcArray, targetSum);
+    
+    expect(foundSets).to.be.an('array');
+    foundSets.should.have.lengthOf(5);
+    expect(foundSets).to.have.deep.members(expArrays);
+
+  });
+
+});
